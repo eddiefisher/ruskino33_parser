5f3ee5c7e00093e7500598213c067b1c52fdd396
Eddie Fisher <eddi.fisher@gmail.com>
2015-03-12 04:50:43 +0300

    renamed Movie.name to Movie.title, added Movie. schedules and tests

--- lib/ruskino33_parser/movie.rb
+++ lib/ruskino33_parser/movie.rb
@@ -1,43 +1,48 @@
 module Ruskino33Parser
   class Movie
-    attr_accessor :name, :url, :date, :sid
-
     # get Nokogiri
     def initialize(movie)
       @movie = movie
     end
 
-    def name
-      @name = @movie.search('.film_desc h3').first.text
+    def title
+      @movie.search('.film_desc h3').first.text
     end
 
     def schedules
-      schedules = []
-      @movie.search('table.sheet tr').each_with_index do |tr, idx|
-        next if idx == 0
-        schedules.push prepare_schedule(tr.search('td'))
+      result = []
+      @movie.search('table.sheet tr').each do |tr|
+        next if tr.search('td').first.text == 'Зал'
+        result += prepare_schedule(tr.search('td'))
       end
-      schedules
+      result
+    end
+
+    def other_days
+      @movie.search('.other_days a').map do |other_day|
+        {
+          date: other_day.text,
+          url: "http://www.ruskino33.ru#{other_day.attribute('href')}"
+        }
+      end
     end
 
     private
 
     def prepare_schedule(cell)
-      {
-        hall: cell.first.text,
-        prices: [
-          { time: prepare_time(cell.last), price: prepare_price },
-          { time: prepare_time(cell.last), price: prepare_price }
-        ]
-      }
+      cell.last.search('a').map do |time|
+        {
+          hall: cell.first.text,
+          time: "#{time.text}:#{time.next.text}",
+          prices: prepare_price(time.attribute('title').text),
+          book_url: "http://www.ruskino33.ru#{time.attribute('href')}#session_info"
+        }
+      end
     end
 
-    def prepare_price
-      { vip: 100, doble_seat: '100', economy: '100' }
-    end
-
-    def prepare_time(cell)
-      "#{cell.search('a').first.text}:#{cell.search('sup').first.text}"
+    def prepare_price(prices)
+      prices = prices.split(';').map { |price| price.sub!('руб', '').to_i }
+      { vip: prices[0], doble_seat: prices[1], economy: prices[2] }
     end
   end
 end
--- spec/movie_spec.rb
+++ spec/movie_spec.rb
@@ -1,9 +1,28 @@
 require 'spec_helper'
 
 describe Ruskino33Parser::Movie do
-  let(:today) { Ruskino33Parser::Anonce.new date: '2015-03-11' }
-  let(:movie) { today.movies.first }
+  let(:anonce) { Ruskino33Parser::Anonce.new test: true }
+  let(:movie) { anonce.movies.first }
 
-  it { expect(movie.name).to eq('Духless 2') }
-  it { expect(movie.schedules).to eq([{ hall: "Большой зал", time: "21:25" }]) }
+  let(:title) { 'Духless 2' }
+  let(:schedules) {
+    [
+      { hall: "Большой зал", time: "20:40", prices: { vip: 200, doble_seat: 150, economy: 150 }, book_url: "http://www.ruskino33.ru/movies/session.shtml?id=1742&sid=53690&date=12.03.2015#session_info" },
+      { hall: "Большой зал", time: "23:00", prices: { vip: 300, doble_seat: 250, economy: 250 }, book_url: "http://www.ruskino33.ru/movies/session.shtml?id=1742&sid=53691&date=12.03.2015#session_info" },
+      { hall: "Малый зал", time: "16:45", prices: { vip: 250, doble_seat: 200, economy: 200 }, book_url: "http://www.ruskino33.ru/movies/session.shtml?id=1742&sid=53563&date=12.03.2015#session_info" }
+    ]
+  }
+  let(:other_days) {
+    url = 'http://www.ruskino33.ru/movies/film.shtml'
+    [
+      { date: "13.03.2015", url: "#{url}?id=1742&date=13.03.2015" }, { date: "14.03.2015", url: "#{url}?id=1742&date=14.03.2015" },
+      { date: "15.03.2015", url: "#{url}?id=1742&date=15.03.2015" }, { date: "16.03.2015", url: "#{url}?id=1742&date=16.03.2015" },
+      { date: "17.03.2015", url: "#{url}?id=1742&date=17.03.2015" }, { date: "18.03.2015", url: "#{url}?id=1742&date=18.03.2015" }
+    ]
+  }
+
+  it { expect(anonce.movies.count).to eq(2) }
+  it { expect(movie.title).to eq(title) }
+  it { expect(movie.schedules).to eq(schedules) }
+  it { expect(movie.other_days).to eq(other_days) }
 end
